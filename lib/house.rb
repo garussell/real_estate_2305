5f047742628161ed378e90ebbb798776de680773
Allen Russell <garyrusselljr@gmail.com>
2023-05-18 10:11:31 -0600

    feat: add rooms and add_rooms methods

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,12 @@
 class House
   attr_reader :price
               :address
+              :rooms
 
   def initialize(price, address)
     @price = price
     @address = address
+    @rooms = []
   end
 
   def price
@@ -13,5 +15,13 @@ class House
 
   def address
     @address
+  end
+
+  def rooms
+    @rooms
+  end
+
+  def add_room(room)
+    @rooms << room
   end
 end
