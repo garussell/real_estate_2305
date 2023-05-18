1b12805316783a979cb291865ece4084706efb64
Allen Russell <garyrusselljr@gmail.com>
2023-05-18 09:57:50 -0600

    feat: add price conversion to_i method

---
+++ lib/house.rb
@@ -0,0 +1,13 @@
+class House
+  attr_reader :price
+              :address
+
+  def initialize(price, address)
+    @price = price
+    @address = address
+  end
+
+  def price
+    @price.delete_prefix("$").to_i
+  end
+end
